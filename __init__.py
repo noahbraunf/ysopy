6ed3d0de40ef47b3a271c36c4e7db0c4312c2246
Noah <Braunfeld>
2020-12-20 21:11:40 -0500

    updated layout

--- __init__.py
+++
@@ -1,1 +0,0 @@
-alksdjlakj
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ysospy-noahbraunf",
+    version="0.0.2",
+    author="Noah Braunfeld",
+    author_email="noah@braunfeld.dev",
+    description="Useful helper functions for HRL",
+    long_description=long_description,
+    url="https://github.com/noahbraunf/ysopy",
+    classifiers=["Programming Language :: Python :: 3"],
+    python_requires=">=3.6",
+)
+
