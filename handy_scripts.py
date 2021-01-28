ea4098416b0b73b159248bdd08ff686577385f48
thissop <68932815+thissop@users.noreply.github.com>
2021-01-28 09:25:19 -0500

    Update handy_scripts.py
    :+1:

--- handy_scripts.py
+++ handy_scripts.py
@@ -106,3 +106,53 @@ def sortFI(data, indices): #indices must be less than or equal to data
     for elem in indices:
         rearranged.append(data[elem])
     return rearranged   
+
+def clean_clusters(dates,paired_lists,tolerance):
+    #Import(s)
+    import numpy as np
+    import math
+
+    #Action
+
+    min_date = math.floor(np.min(dates))
+    max_date = math.ceil(np.max(dates))
+    date_bins = list(range(min_date,max_date,1))
+    binned_dates = np.digitize(x=dates,bins=date_bins)
+    bin_counts = np.bincount(binned_dates)
+
+    nz_bin_counts = bin_counts[np.nonzero(bin_counts)] #Get rid of bin_counts = 0 for calculations
+
+    mean_counts = np.mean(nz_bin_counts)
+    std_counts = np.std(nz_bin_counts,ddof=1)
+
+    cutoff = mean_counts+float(tolerance)*(std_counts)
+
+    intervals_to_remove = []
+    
+    bin_counts = list(bin_counts)
+
+    for count in bin_counts:
+        if count > cutoff:
+            bin_index = bin_counts.index(count)
+            lower_edge = date_bins[bin_index-1]
+            upper_edge = date_bins[bin_index]
+            edge_string = str(lower_edge)+':'+str(upper_edge)
+            intervals_to_remove.append(edge_string)
+
+    indices_to_remove = []
+
+    for interval in intervals_to_remove:
+        interval_list = interval.split(':')
+        lower = float(interval_list[0])
+        upper = float(interval_list[1])
+        for date in dates:
+            if date > lower and date < upper:
+                date_index = dates.index(date)
+                indices_to_remove.append(date_index)
+
+    for i in sorted(indices_to_remove, reverse=True):
+        del dates[i]
+        for paired_list in paired_lists:
+            del paired_list[i]
+
+    return dates, paired_lists
