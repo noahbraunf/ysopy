e2ac37acf0396d4e4c5a98a05dc1662c57b9fb22
thissop <68932815+thissop@users.noreply.github.com>
2021-04-01 13:58:42 -0400

    NOT 100% PYTHON ANYMORE

--- visual_vetting.sh
+++ visual_vetting.sh
@@ -1,1 +1,10 @@
 # Purpose: neat sh program to display images in same window, close them, and allow user to classify them
+
+sudo apt-get install fim
+for i in 1 2 3 4 5 6 6 #replace these numbers with an array of ids
+  do 
+  fim #image path
+  read varname
+  echo $i, $varname
+  done
+ 
